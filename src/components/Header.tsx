ff74b6109b760395e3b49362df0d05d237f3fa33
Berzan <berzan@tutanota.com>
2023-10-09 20:53:01 +0000

    use: escape

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="sticky flex items-center justify-center gap-3 px-4 text-green-300 border-b h-14 border-neutral-600 bg-neutral-950">
-            <a className="text-xl font-bold">Berzan's Programming Language</a>
+            <a className="text-xl font-bold">Berzan&#39;s Programming Language</a>
         </header>
     )
 }
