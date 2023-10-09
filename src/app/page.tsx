511c77931c81a5b027d4497a4c561f4d64a82f2c
Berzan <berzan@tutanota.com>
2023-10-09 20:37:18 +0000

    add: home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+"use client"
+import { Header } from '@/components/Header'
+import { Repl } from '@/components/Repl'
 
 export default function Home() {
   return (
-    <main>
-
-    </main>
+    <div className='flex flex-col'>
+      <Header />
+      <main className='flex flex-col self-center w-full max-w-2xl p-6'>
+        <Repl />
+      </main>
+    </div>
   )
 }
+
